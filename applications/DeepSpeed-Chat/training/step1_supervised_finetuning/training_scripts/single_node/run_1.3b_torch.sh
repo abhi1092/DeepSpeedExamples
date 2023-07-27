0b2570f4f830b6d3412164349b4ec8fd4ffe6432
Abhishek Bhandwaldar <abhi1092@gmail.com>
2023-07-27 09:28:39 -0400

    fix

--- applications/DeepSpeed-Chat/training/step1_supervised_finetuning/training_scripts/single_node/run_1.3b_torch.sh
+++ applications/DeepSpeed-Chat/training/step1_supervised_finetuning/training_scripts/single_node/run_1.3b_torch.sh
@@ -14,11 +14,7 @@ fi
 mkdir -p $OUTPUT
 NEW_PORT=23457
 
-python3 -m torch.distributed.run \
-   --nproc_per_node 1 --nnodes ${WORLD_SIZE}  \
-   --rdzv_id=101 \
-   --rdzv_endpoint="abhi-deepspeed-1-worker-0:$NEW_PORT" \
-   --rdzv_backend=c10d \
+torchrun --nnodes=${WORLD_SIZE} --node_rank=${RANK} --nproc_per_node=1 --rdzv_id=102 --rdzv_endpoint="${MASTER_ADDR}:${NEW_PORT}" \
     main.py \
    --data_path Dahoas/rm-static Dahoas/full-hh-rlhf Dahoas/synthetic-instruct-gptj-pairwise yitingxie/rlhf-reward-datasets \
    --data_split 2,4,4 \
