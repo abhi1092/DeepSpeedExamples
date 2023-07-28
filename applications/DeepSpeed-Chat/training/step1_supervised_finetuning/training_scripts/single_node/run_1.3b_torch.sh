f3a2c9ad4d9ed155c62912c47b9fb7404319c47e
Abhishek Bhandwaldar <abhi1092@gmail.com>
2023-07-27 20:12:50 -0400

    fix

--- applications/DeepSpeed-Chat/training/step1_supervised_finetuning/training_scripts/single_node/run_1.3b_torch.sh
+++ applications/DeepSpeed-Chat/training/step1_supervised_finetuning/training_scripts/single_node/run_1.3b_torch.sh
@@ -17,7 +17,30 @@ NEW_PORT=23457
 #    test.py
 
 
-torchrun --nnodes=1 --node_rank=${RANK} --nproc_per_node=2 --rdzv_id=102 --rdzv_endpoint="${MASTER_ADDR}:${NEW_PORT}" \
+#torchrun --nnodes=${WORLD_SIZE} --node_rank=${RANK} --nproc_per_node=2 --rdzv_id=102 --rdzv_endpoint="${MASTER_ADDR}:${NEW_PORT}" \
+#    main.py \
+#   --data_path Dahoas/rm-static Dahoas/full-hh-rlhf Dahoas/synthetic-instruct-gptj-pairwise yitingxie/rlhf-reward-datasets \
+#   --data_split 2,4,4 \
+#   --model_name_or_path facebook/opt-1.3b \
+#   --per_device_train_batch_size 8 \
+#   --per_device_eval_batch_size 8 \
+#   --data_output_path /app/tmp \
+#   --max_seq_len 512 \
+#   --learning_rate 9.65e-6 \
+#   --weight_decay 0. \
+#   --num_train_epochs 16 \
+#   --gradient_accumulation_steps 1 \
+#   --lr_scheduler_type cosine \
+#   --num_warmup_steps 0 \
+#   --seed 1234 \
+#   --zero_stage $ZERO_STAGE \
+#   --deepspeed \
+#   --enable_tensorboard \
+#   --tensorboard_path $OUTPUT \
+#   --output_dir $OUTPUT
+
+
+torchrun --nnodes=1 --node_rank=0 --nproc_per_node=2 --rdzv_id=102 --rdzv_endpoint="${MASTER_ADDR}:${NEW_PORT}" \
     main.py \
    --data_path Dahoas/rm-static Dahoas/full-hh-rlhf Dahoas/synthetic-instruct-gptj-pairwise yitingxie/rlhf-reward-datasets \
    --data_split 2,4,4 \
