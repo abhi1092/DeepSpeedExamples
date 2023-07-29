df0b6611be54ecfe678dfe677ee72d6e5d321c84
Abhishek Bhandwaldar <abhi1092@gmail.com>
2023-07-28 23:27:47 -0400

    fix

--- applications/DeepSpeed-Chat/training/step1_supervised_finetuning/test.py
+++ applications/DeepSpeed-Chat/training/step1_supervised_finetuning/test.py
@@ -1,41 +1,42 @@
+import os
+import argparse
+
 import torch
 import torch.distributed as dist
-import torch.nn as nn
-import torch.optim as optim
 
-from torch.nn.parallel import DistributedDataParallel as DDP
-
-class ToyModel(nn.Module):
-    def __init__(self):
-        super(ToyModel, self).__init__()
-        self.net1 = nn.Linear(10, 10)
-        self.relu = nn.ReLU()
-        self.net2 = nn.Linear(10, 5)
-
-    def forward(self, x):
-        return self.net2(self.relu(self.net1(x)))
+# Environment variables set by torch.distributed.launch
+LOCAL_RANK = int(os.environ['LOCAL_RANK'])
+WORLD_SIZE = int(os.environ['WORLD_SIZE'])
+WORLD_RANK = int(os.environ['RANK'])
 
 
-def demo_basic():
-    dist.init_process_group("nccl")
-    rank = dist.get_rank()
-    print(f"Start running basic DDP example on rank {rank}.")
+def run(backend):
+    tensor = torch.zeros(1)
 
-    # create model and move it to GPU with id rank
-    device_id = rank % torch.cuda.device_count()
-    model = ToyModel().to(device_id)
-    ddp_model = DDP(model, device_ids=[device_id])
+    # Need to put tensor on a GPU device for nccl backend
+    if backend == 'nccl':
+        device = torch.device("cuda:{}".format(LOCAL_RANK))
+        tensor = tensor.to(device)
 
-    loss_fn = nn.MSELoss()
-    optimizer = optim.SGD(ddp_model.parameters(), lr=0.001)
-    for i in range(1000):
-        optimizer.zero_grad()
-        outputs = ddp_model(torch.randn(20, 10))
-        labels = torch.randn(20, 5).to(device_id)
-        loss = loss_fn(outputs, labels)
-        print(f"running basic DDP example on rank {rank}. Loss {loss}")
-        loss.backward()
-        optimizer.step()
+    if WORLD_RANK == 0:
+        for rank_recv in range(1, WORLD_SIZE):
+            dist.send(tensor=tensor, dst=rank_recv)
+            print('worker_{} sent data to Rank {}\n'.format(0, rank_recv))
+    else:
+        dist.recv(tensor=tensor, src=0)
+        print('worker_{} has received data from rank {}\n'.format(WORLD_RANK, 0))
+
+
+def init_processes(backend):
+    dist.init_process_group(backend, rank=WORLD_RANK, world_size=WORLD_SIZE)
+    run(backend)
+
 
 if __name__ == "__main__":
-    demo_basic()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--local_rank", type=int,
+                        help="Local rank. Necessary for using the torch.distributed.launch utility.")
+    parser.add_argument("--backend", type=str, default="nccl", choices=['nccl', 'gloo'])
+    args = parser.parse_args()
+
+    init_processes(backend=args.backend)
