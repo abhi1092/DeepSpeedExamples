83d1bdc8e233d894ea0855ae81127f69aeaf0716
Your Name <you@example.com>
2023-08-04 03:35:17 -0400

    doing profiling in the first epoch?

--- applications/DeepSpeed-Chat/training/utils/ds_utils.py
+++ applications/DeepSpeed-Chat/training/utils/ds_utils.py
@@ -69,7 +69,7 @@ def get_train_ds_config(offload,
         },
         "flops_profiler": {
             "enabled": True,
-            "profile_step": 1,
+            "profile_step": 0,
             "module_depth": -1,
             "top_modules": 1,
             "detailed": True,
