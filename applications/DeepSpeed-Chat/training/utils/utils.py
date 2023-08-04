03de861e5440b8d5d49da395c50d4ae06e16e79f
Your Name <you@example.com>
2023-08-04 02:33:38 -0400

    fixed loading the tokenizer

--- applications/DeepSpeed-Chat/training/utils/utils.py
+++ applications/DeepSpeed-Chat/training/utils/utils.py
@@ -43,14 +43,19 @@ class MovingAverage:
 
 
 def load_hf_tokenizer(model_name_or_path, fast_tokenizer=True):
+    from huggingface_hub.utils._validators import HFValidationError
     if os.path.exists(model_name_or_path):
         # Locally tokenizer loading has some issue, so we need to force download
         model_json = os.path.join(model_name_or_path, "config.json")
         if os.path.exists(model_json):
             model_json_file = json.load(open(model_json))
             model_name = model_json_file["_name_or_path"]
-            tokenizer = AutoTokenizer.from_pretrained(
-                model_name, fast_tokenizer=fast_tokenizer)
+            try:
+                tokenizer = AutoTokenizer.from_pretrained(
+                    model_name, fast_tokenizer=fast_tokenizer)
+            except HFValidationError:
+                tokenizer = AutoTokenizer.from_pretrained(
+                    model_name_or_path, fast_tokenizer=fast_tokenizer)
     else:
         tokenizer = AutoTokenizer.from_pretrained(
             model_name_or_path, fast_tokenizer=fast_tokenizer)
