2a17fb7f2568f7aa4c07c8b08d3a5c9107f13baf
Your Name <you@example.com>
2023-08-06 22:40:56 -0400

    added registration of the pithya model

--- applications/DeepSpeed-Chat/training/utils/model/model_utils.py
+++ applications/DeepSpeed-Chat/training/utils/model/model_utils.py
@@ -23,8 +23,7 @@ def create_hf_model(model_class,
                     rlhf_training=False,
                     disable_dropout=False):
     #embed an ipython and print the file and line number when doing so
-    from IPython import embed; embed(using=False, header=get_caller())
-    if "oasst" in model_name_or_path:
+    if "oasst" in model_name_or_path: #this is necessary to load the oasst model
         import model_training.models.reward_model
     model_config = AutoConfig.from_pretrained(model_name_or_path)
     if disable_dropout:
