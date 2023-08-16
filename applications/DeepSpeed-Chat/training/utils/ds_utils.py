c82a44c188202c931d0ec5f3354b4c200e2954a4
Your Name <you@example.com>
2023-08-16 04:55:23 -0400

    memory issues

--- applications/DeepSpeed-Chat/training/utils/ds_utils.py
+++ applications/DeepSpeed-Chat/training/utils/ds_utils.py
@@ -38,12 +38,12 @@ def get_train_ds_config(offload,
         # "stage3_param_persistence_threshold": 1e6,
         # "stage3_max_live_parameters": 1e9,
         # "stage3_prefetch_bucket_size": 5e8,
-          "allgather_partitions": true,
+          "allgather_partitions": True,
         "allgather_bucket_size": 5e8,
-        "overlap_comm": false,
-        "reduce_scatter": true,
+        "overlap_comm": False,
+        "reduce_scatter": True,
         "reduce_bucket_size": 5e8,
-        "contiguous_gradients": false
+        "contiguous_gradients": False
     }
     # "fp16": {
     #     "enabled": True,
