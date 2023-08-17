8d135d0ca3cee05c2a3d3b52efa9f3bfd0c73bd9
Your Name <you@example.com>
2023-08-17 06:46:22 -0400

    why the model doesn't save?

--- applications/DeepSpeed-Chat/training/utils/ds_utils.py
+++ applications/DeepSpeed-Chat/training/utils/ds_utils.py
@@ -64,13 +64,13 @@ def get_train_ds_config(offload,
         "train_micro_batch_size_per_gpu": MICRO_BATCH_SIZE,
         "steps_per_print": 10,
         "zero_optimization": zero_opt_dict,
-        # "bf16": {
-        #     "enabled": True
-        # },
-        "fp16": {
-            "enabled": True,
-            "loss_scale_window": 100
+        "bf16": {
+            "enabled": True
         },
+        # "fp16": {
+        #     "enabled": True,
+        #     "loss_scale_window": 100
+        # },
         "gradient_clipping": 1.0,
         "prescale_gradients": False,
         "wall_clock_breakdown": True,
