5eb38ea57a329509884f4478ecaed95efff0a7fa
Abhishek Bhandwaldar <abhi1092@gmail.com>
2023-08-17 19:24:45 -0400

    fix

--- applications/DeepSpeed-Chat/training/utils/data/data_utils.py
+++ applications/DeepSpeed-Chat/training/utils/data/data_utils.py
@@ -172,13 +172,15 @@ class PromptDataset(Dataset):
 
 
 def create_label(tokens, tokenizer, raw_dataset):
-    tokens["labels"] = tokens["input_ids"]
+    tokens["labels"] = tokens["input_ids"].clone()
     response_token_ids = tokenizer.encode(raw_dataset.ASSISTANT_KEY)
     assert len(response_token_ids) == 1, "Tokenizer does not have special token"
     response_token_ids_start_idx = None
     for idx in np.where(tokens["labels"][0] == response_token_ids[0])[0]:
         response_token_ids_start_idx = idx
     assert response_token_ids_start_idx is not None, "Could not find response key"
+    print(response_token_ids_start_idx)
+    print(tokens["labels"].shape)
     tokens["labels"][:response_token_ids_start_idx] = -100
     print(f"{response_token_ids_start_idx=}")
     print("=================")
