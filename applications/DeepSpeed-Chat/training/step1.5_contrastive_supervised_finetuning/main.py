0ac114143f75e6d1dba3f2177d64ede8bfbf5756
Abhishek Bhandwaldar <abhi1092@gmail.com>
2023-08-17 19:17:41 -0400

    fix

--- applications/DeepSpeed-Chat/training/step1.5_contrastive_supervised_finetuning/main.py
+++ applications/DeepSpeed-Chat/training/step1.5_contrastive_supervised_finetuning/main.py
@@ -215,7 +215,7 @@ def main():
     print(f"All reduce test 1 on global rank {args.global_rank} rank {args.local_rank}")
     torch.distributed.barrier()
     # tokenizer = load_hf_tokenizer(args.model_name_or_path, fast_tokenizer=True, load_from_local_file=True)
-    tokenizer = load_hf_tokenizer("/new_data/rl-4-llm/granite_models/instruction_tuned/granite-13b-sft-mix700k/ckpt-1000bn-base", fast_tokenizer=True, load_from_local_file=True)
+    tokenizer = load_hf_tokenizer("/new_data/rl-4-llm/experiment_alignment/granite13b_1000bn/cft_wadolly_100k_tulu_e2_beta_1e-6_base_700k_sft", fast_tokenizer=True, load_from_local_file=True)
     tokenizer.pad_token = tokenizer.eos_token
     # make sure tokenizer is right pad in our logic
     tokenizer.padding_side = 'right'
