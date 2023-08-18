45689472570b09df9a1e6d1e43a463b9f65312a3
Abhishek Bhandwaldar <abhi1092@gmail.com>
2023-08-18 18:51:31 -0400

    fix

--- applications/DeepSpeed-Chat/training/step1.5_contrastive_supervised_finetuning/training_scripts/single_node/run_1.3b_torch.sh
+++ applications/DeepSpeed-Chat/training/step1.5_contrastive_supervised_finetuning/training_scripts/single_node/run_1.3b_torch.sh
@@ -75,7 +75,7 @@ torchrun --nnodes=1 --node_rank=0 --nproc_per_node=8 --rdzv_id=107 --rdzv_endpoi
    --learning_rate 1e-4 \
    --weight_decay 0.1 \
    --num_train_epochs 1  \
-   --gradient_accumulation_steps 1 \
+   --gradient_accumulation_steps 4 \
    --lr_scheduler_type cosine \
    --num_warmup_steps 0 \
    --seed 1234 \
