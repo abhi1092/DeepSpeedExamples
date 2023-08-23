ca0a8127aff973dad8f441804a12c69ae6952e0b
Abhishek Bhandwaldar <abhi1092@gmail.com>
2023-08-23 16:13:56 -0400

    fix

--- applications/DeepSpeed-Chat/training/step1_supervised_finetuning/training_scripts/run_1.3b_torch.sh
+++ applications/DeepSpeed-Chat/training/step1_supervised_finetuning/training_scripts/run_1.3b_torch.sh
@@ -69,7 +69,7 @@ torchrun --nnodes=1 --node_rank=0 --nproc_per_node=8 --rdzv_id=107 --rdzv_endpoi
     main.py \
    --data_path dolly_dataset \
    --data_split 1,0,0,0 \
-   --model_name_or_path  decapoda-research/llama-7b-hf \
+   --model_name_or_path  /new_data/rl-4-llm/dpc_alignment/tulu_paper_reproduction/llama-7b-dolly-test \
    --per_device_train_batch_size 4 \
    --per_device_eval_batch_size 4 \
    --data_output_path ./data \
