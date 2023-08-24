afc483508c8b55ab73998ead02bd78854dc9e0e3
Abhishek Bhandwaldar <abhi1092@gmail.com>
2023-08-24 10:18:01 -0400

    adding torchrun support

--- applications/DeepSpeed-Chat/training/step1_supervised_finetuning/training_scripts/run_torch.sh
+++ applications/DeepSpeed-Chat/training/step1_supervised_finetuning/training_scripts/run_torch.sh
@@ -69,7 +69,7 @@ torchrun --nnodes=1 --node_rank=0 --nproc_per_node=8 --rdzv_id=107 --rdzv_endpoi
     main.py \
    --data_path dolly_dataset \
    --data_split 1,0,0,0 \
-   --model_name_or_path  decapoda-research/llama-7b-hf \
+   --model_name_or_path  facebook/opt-1.3b \
    --per_device_train_batch_size 4 \
    --per_device_eval_batch_size 4 \
    --data_output_path ./data \
