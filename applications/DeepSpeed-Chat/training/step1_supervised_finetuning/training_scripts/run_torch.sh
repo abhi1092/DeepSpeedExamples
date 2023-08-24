b7e3a9389dcfd549f014ec17cc057c91f0b99ac9
Abhishek Bhandwaldar <abhi1092@gmail.com>
2023-08-24 10:59:45 -0400

    fix

--- applications/DeepSpeed-Chat/training/step1_supervised_finetuning/training_scripts/run_torch.sh
+++ applications/DeepSpeed-Chat/training/step1_supervised_finetuning/training_scripts/run_torch.sh
@@ -83,6 +83,7 @@ torchrun --nnodes=1 --node_rank=0 --nproc_per_node=8 --rdzv_id=107 --rdzv_endpoi
    --warmup_percentage 0.03 \
    --seed 1234 \
    --gradient_checkpointing \
+   --offload \
    --zero_stage 3 \
    --deepspeed \
    --output_dir $base_dir/$experiment_dir
