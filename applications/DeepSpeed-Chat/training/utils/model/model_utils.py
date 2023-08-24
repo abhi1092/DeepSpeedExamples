aa720c257e75f98e50d477c3997d990f4a20f41f
Abhishek Bhandwaldar <abhi1092@gmail.com>
2023-08-23 23:46:56 -0400

    fix

--- applications/DeepSpeed-Chat/training/utils/model/model_utils.py
+++ applications/DeepSpeed-Chat/training/utils/model/model_utils.py
@@ -41,14 +41,10 @@ def create_hf_model(model_class,
         # the weight loading is handled by create critic model
         model = model_class.from_config(model_config)
     else:
-        if load_from_local_file:
-            model = model_class.from_pretrained(
-                model_name_or_path)
-        else:
-            model = model_class.from_pretrained(
-                model_name_or_path,
-                from_tf=bool(".ckpt" in model_name_or_path),
-                config=model_config)
+        model = model_class.from_pretrained(
+            model_name_or_path,
+            from_tf=bool(".ckpt" in model_name_or_path),
+            config=model_config)
 
     model.config.end_token_id = tokenizer.eos_token_id
     model.config.pad_token_id = model.config.eos_token_id
