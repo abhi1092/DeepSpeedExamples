4c9671a560329f21fad02c9eeb14ff06a865c0ec
Abhishek Bhandwaldar <abhi1092@gmail.com>
2023-08-25 11:04:58 -0400

    adding cft model

---
+++ applications/DeepSpeed-Chat/training/utils/model/cft_model.py
@@ -0,0 +1,182 @@
+# Copyright (c) Microsoft Corporation.
+# SPDX-License-Identifier: Apache-2.0
+
+# DeepSpeed Team
+import torch
+from torch import nn
+from torch.nn import CrossEntropyLoss
+import torch.nn.functional as F
+
+## Note that the following code is modified from
+## https://github.com/CarperAI/trlx/blob/main/examples/summarize_rlhf/reward_model/reward_model.py
+class CftModel(nn.Module):
+
+    def __init__(self, base_model, tokenizer, total_steps, beta=1e-6, no_beta_decay=False, num_padding_at_beginning=0):
+        super().__init__()
+        self.config = base_model.config
+        self.num_padding_at_beginning = num_padding_at_beginning
+        self.cfttranrsformer = base_model
+        self.global_step = 0
+        self.no_beta_decay = no_beta_decay
+        self.beta_initial, self.beta_final, self.beta_step = beta, 0.0, total_steps
+        self.beta_warmup = 0
+        self.PAD_ID = tokenizer.pad_token_id
+
+    def gradient_checkpointing_enable(self):
+        self.cfttranrsformer.gradient_checkpointing_enable()
+
+    def gradient_checkpointing_disable(self):
+        self.cfttranrsformer.gradient_checkpointing_disable()
+
+    def get_loss(self, lm_logits, labels):
+        # labels = labels.to(lm_logits.device)
+        # we are doing next-token prediction; shift prediction scores and input ids by one
+        shift_logits = lm_logits[:, :-1, :].contiguous()
+        labels = labels[:, 1:].contiguous()
+        loss_fct = CrossEntropyLoss()
+        lm_loss = loss_fct(shift_logits.view(-1, shift_logits.size(-1)), labels.view(-1))
+        return lm_loss
+
+    def forward(self,
+                input_ids=None,
+                past_key_values=None,
+                attention_mask=None,
+                position_ids=None,
+                head_mask=None,
+                inputs_embeds=None,
+                use_cache=False,
+                labels=None,
+                use_negative_data=None):
+        loss = None
+
+        output = self.cfttranrsformer(
+            input_ids,
+            past_key_values=past_key_values,
+            attention_mask=attention_mask,
+            head_mask=head_mask,
+            inputs_embeds=inputs_embeds,
+            use_cache=use_cache)
+
+        lm_logits = output.logits
+        # rewards = self.v_head(hidden_states).squeeze(-1)
+        # chosen_mean_scores = []
+        # rejected_mean_scores = []
+
+        # Split the inputs and rewards into two parts, chosen and rejected
+        assert use_negative_data is not None
+        assert len(input_ids.shape) == 2
+        bs = input_ids.shape[0] // 2
+        seq_len = input_ids.shape[1]
+
+        # chosen_ids = input_ids[:bs]  # bs x seq x 1
+        # rejected_ids = input_ids[bs:]
+        chosen_lm_logits = lm_logits[:bs]
+        rejected_lm_logits = lm_logits[bs:]
+        chosen_labels = labels[:bs]
+        rejected_labels = labels[bs:]
+        use_negative_data_rejected = use_negative_data[bs:]
+        rejected_lm_logits = rejected_lm_logits[use_negative_data_rejected == 1, :]
+        rejected_labels = rejected_labels[use_negative_data_rejected == 1, :]
+
+        pos_loss = self.get_loss(chosen_lm_logits, chosen_labels)
+        if rejected_lm_logits.nelement() != 0 and self.beta_initial != 0.0:
+            self.global_step += 1
+            rejected_lm_logits = -F.logsigmoid(-rejected_lm_logits)
+            neg_loss = self.get_loss(rejected_lm_logits, rejected_labels)
+            if self.no_beta_decay:
+                beta = self.beta_initial
+            else:
+                if self.beta_warmup != -1 and self.global_step < self.beta_warmup:
+                    beta = 0.00  # Use very low beta initially
+                else:
+                    beta = linear_decay(self.beta_initial, self.beta_final, self.beta_step, self.global_step)
+            loss = pos_loss - beta * neg_loss
+        else:
+            loss = pos_loss
+
+        # for i in range(bs):
+        #     chosen_id = chosen_ids[i]
+        #     rejected_id = rejected_ids[i]
+        #     chosen_reward = chosen_rewards[i]
+        #     rejected_reward = rejected_rewards[i]
+        #
+        #     c_inds = (chosen_id == self.PAD_ID).nonzero()
+        #     c_ind = c_inds[self.num_padding_at_beginning].item() if len(
+        #         c_inds
+        #     ) > self.num_padding_at_beginning else seq_len  # OPT model pads the first token, so we need to use the second padding token as the end of the sequence
+        #     check_divergence = (chosen_id != rejected_id).nonzero()
+        #
+        #     if len(check_divergence) == 0:
+        #         end_ind = rejected_reward.size(-1)
+        #         divergence_ind = end_ind - 1
+        #         r_ind = c_ind
+        #     else:
+        #         # Check if there is any padding otherwise take length of sequence
+        #         r_inds = (rejected_id == self.PAD_ID).nonzero()
+        #         r_ind = r_inds[self.num_padding_at_beginning].item(
+        #         ) if len(r_inds) > self.num_padding_at_beginning else seq_len
+        #         end_ind = max(c_ind, r_ind)
+        #         divergence_ind = check_divergence[0]
+        #     assert divergence_ind > 0
+        #     c_truncated_reward = chosen_reward[divergence_ind:end_ind]
+        #     r_truncated_reward = rejected_reward[divergence_ind:end_ind]
+        #     chosen_mean_scores.append(
+        #         chosen_reward[c_ind - 1])  #use the end score for reference
+        #     rejected_mean_scores.append(rejected_reward[r_ind - 1])
+        #
+        #     loss += -torch.nn.functional.logsigmoid(c_truncated_reward -
+        #                                             r_truncated_reward).mean()
+
+        loss = loss / bs
+        return {
+            "loss": loss,
+        }
+
+    # def forward_value(self,
+    #                   input_ids=None,
+    #                   attention_mask=None,
+    #                   past_key_values=None,
+    #                   position_ids=None,
+    #                   head_mask=None,
+    #                   inputs_embeds=None,
+    #                   return_value_only=False,
+    #                   prompt_length=0,
+    #                   use_cache=False):
+    #
+    #     transformer_outputs = self.cfttranrsformer(
+    #         input_ids,
+    #         past_key_values=past_key_values,
+    #         attention_mask=attention_mask,
+    #         head_mask=head_mask,
+    #         inputs_embeds=inputs_embeds,
+    #         use_cache=use_cache)
+    #     hidden_states = transformer_outputs[0]
+    #     values = self.v_head(hidden_states).squeeze(-1)
+    #     if return_value_only:
+    #         return values
+    #     else:
+    #         # [0 0 0 0 prompt, answer, 0 0 0 0 ] for step 3, we have padding at the beginning
+    #         # [prompt, answer, 0, 0, 0, 0] this is normal
+    #         assert prompt_length > 1, "prompt_length must be greater than 1 to help select the end score"
+    #         bs = values.size(0)
+    #         seq_len = input_ids.shape[1]
+    #         chosen_end_scores = [
+    #         ]  # we use this name for consistency with the original forward function
+    #         for i in range(bs):
+    #             input_id = input_ids[i]
+    #             value = values[i]
+    #
+    #             c_inds = (input_id[prompt_length:] == self.PAD_ID).nonzero()
+    #             # here we only use the answer part of the sequence so we do not need to care about the padding at the beginning
+    #             c_ind = c_inds[0].item() + prompt_length if len(
+    #                 c_inds) > 0 else seq_len
+    #             chosen_end_scores.append(value[c_ind - 1])
+    #         return {
+    #             "values": values,
+    #             "chosen_end_scores": torch.stack(chosen_end_scores),
+    #         }
+
+def linear_decay(initial_value, final_value, decay_steps, current_step):
+    decay_rate = (initial_value - final_value) / decay_steps
+    decayed_value = initial_value - decay_rate * current_step
+    return max(decayed_value, final_value)
