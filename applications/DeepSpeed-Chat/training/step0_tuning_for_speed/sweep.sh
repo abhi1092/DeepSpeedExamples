8e3dc969ccb5d71708762ff027f223501c6eb783
Your Name <you@example.com>
2023-08-31 11:41:41 -0500

    adding sweeps

--- applications/DeepSpeed-Chat/training/step0_tuning_for_speed/sweep.sh
+++ applications/DeepSpeed-Chat/training/step0_tuning_for_speed/sweep.sh
@@ -4,10 +4,10 @@ MODEL_PATH=/new_data/rl-4-llm/granite_models/pretrained/step_225000_ckpt/
 DS_CONFIG_PATH=ds_config_z3.json
 
 deepspeed --autotuning tune --num_nodes=1 --num_gpus=8 $CLMPATH \
---deepspeed $DS_CONFIG_PATH \
---model_name_or_path $MODEL_PATH \
---do_train \
---do_eval \
---fp16 \
---per_device_train_batch_size 2 \
---gradient_accumulation_steps 1
+  --deepspeed $DS_CONFIG_PATH \
+  --model_name_or_path $MODEL_PATH \
+  --do_train \
+  --do_eval \
+  --fp16 \
+  --per_device_train_batch_size 2 \
+  --gradient_accumulation_steps 1
