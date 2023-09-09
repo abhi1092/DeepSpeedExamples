832f3c2d1c73ab48b43c4332c39a043ad5143a1c
Your Name <you@example.com>
2023-09-09 03:32:30 -0400

    GPTMegatron

--- applications/DeepSpeed-Chat/training/utils/model/model_utils.py
+++ applications/DeepSpeed-Chat/training/utils/model/model_utils.py
@@ -44,8 +44,9 @@ def create_hf_model(model_class,
     else:
         model = model_class.from_pretrained(
             model_name_or_path,
-            from_tf=bool(".ckpt" in model_name_or_path),
-            config=model_config)
+            # from_tf=bool(".ckpt" in model_name_or_path),
+            # config=model_config
+            )
 
     model.config.end_token_id = tokenizer.eos_token_id
     model.config.pad_token_id = model.config.eos_token_id
