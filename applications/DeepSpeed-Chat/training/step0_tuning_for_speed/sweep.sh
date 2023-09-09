f33b3599acd5565a77db9f2e00f4bd88d99b1bc7
Your Name <you@example.com>
2023-09-08 20:30:30 -0400

    fixed sweep, need to specify chosen key properly

--- applications/DeepSpeed-Chat/training/step0_tuning_for_speed/sweep.sh
+++ applications/DeepSpeed-Chat/training/step0_tuning_for_speed/sweep.sh
@@ -4,7 +4,8 @@ git pull && deepspeed --autotuning tune --num_nodes=1 --num_gpus=8 \
  main.py\
     --model_name_or_path /new_data/rl-4-llm/granite_models/pretrained/step_225000_ckpt/\
   --data_path /new_data/datasets/summarization/tldr_sft_train_117k.jsonl \
-  --prompt_column_name formatted_input\
+  --prompt formatted_input\
+  --chosen summary\
   --data_split 1,10,10\
   --data_output_path /app/.local_data/tuning \
   --max_seq_len 2048\
@@ -13,7 +14,8 @@ git pull && deepspeed --autotuning tune --num_nodes=1 --num_gpus=8 \
   --learning_rate 5e-5\
   --weight_decay 0.01\
   --deepspeed\
-  --deepspeed_config ds_config_z3.json
+  --deepspeed_config ds_config_z3.json\
+  --offload
 
   --deepspeed_config 
 
