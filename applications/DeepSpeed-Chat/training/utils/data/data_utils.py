acf01a7bc288e9f7bb53906f1c5a336b5ece4cd1
Your Name <you@example.com>
2023-09-14 22:25:24 -0400

    debugging

--- applications/DeepSpeed-Chat/training/utils/data/data_utils.py
+++ applications/DeepSpeed-Chat/training/utils/data/data_utils.py
@@ -174,6 +174,7 @@ def create_dataset_split(current_dataset, raw_dataset, train_phase, tokenizer,
             if chosen_sentence is not None:
                 #print end of conversation token
                 print_rank_0(f'chosen_sentence: {chosen_sentence}', color="GREEN")
+                print_rank_0(f'end_of_conversation_token: {end_of_conversation_token}', color='GREEN')
                 exit()
                 chosen_sentence += end_of_conversation_token
                 chosen_token = tokenizer(chosen_sentence,
